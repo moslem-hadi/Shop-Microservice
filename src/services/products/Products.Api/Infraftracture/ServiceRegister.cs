12c28fd56bdf967f48c461fb7000230565369cd8
Moslem Hadi <moslem.hady@gmail.com>
2022-07-02 15:26:14 +0430

    add static class to register services

---
+++ src/services/products/Products.Api/Infraftracture/ServiceRegister.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Products.Infrastructure;
+
+namespace Products.Api.Infraftracture
+{
+    public static class ServiceRegister
+    {
+        public static IServiceCollection AddServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<ProductDbContext>(option =>
+                option.UseNpgsql(builder.Configuration.GetConnectionString("ProductDbConn")));
+
+            return builder.Services;
+        }
+    }
+}
--- src/services/products/Products.Api/Program.cs
+++ src/services/products/Products.Api/Program.cs
@@ -1,32 +1,20 @@
-using Microsoft.EntityFrameworkCore;
-using Products.Infrastructure;
+using Products.Api.Infraftracture;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    // Add services to the container.
-    builder.Services.AddControllers();
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-
-    builder.Services.AddDbContext<ProductDbContext>(option =>
-        option.UseNpgsql(builder.Configuration.GetConnectionString("ProductDbConn")));
-
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-{
-    var app = builder.Build();
+app.UseAuthorization();
 
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+app.MapControllers();
 
-    app.UseAuthorization();
-
-    app.MapControllers();
-
-    app.Run();
-}
+app.Run();
